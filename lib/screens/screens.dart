5140c9aa04d9d25e0b27b7d57f7cfc00836d2524
SRH8 <sergiofraga.cs@gmail.com>
2021-12-26 15:34:00 +0100

    Routes and screens definitions

---
+++ lib/screens/screens.dart
@@ -0,0 +1,11 @@
+export 'package:sky_chat_app/screens/onboard_screen.dart';
+export 'package:sky_chat_app/screens/login_screen.dart';
+export 'package:sky_chat_app/screens/sign_up_screen.dart';
+export 'package:sky_chat_app/screens/chat_list_screen.dart';
+
+export 'package:sky_chat_app/screens/chat_screen.dart';
+
+
+
+
+
